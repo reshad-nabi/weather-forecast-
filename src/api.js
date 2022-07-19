5a0a7b6c9874c92fca97936a094320324de14bb0
reshad-nabi <78980930+reshad-nabi@users.noreply.github.com>
2022-07-19 21:20:12 +0200

    initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "8ed2299114mshb67c8560aa4647dp155a60jsn34ca55f6dc23",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "afcb1b7664f4ac812cea074d956ddd0c";
